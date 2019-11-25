62904ea69cd57b12f250542c0c08aaaa691e7271
webromko <mylogin222@gmail.com>
2019-11-25 18:04:40 +0200

    test

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import "./styles/index.css";
 import App from './application';
-
+//τττ
 ReactDOM.render(
 	<App />,
 	document.getElementById('root')
